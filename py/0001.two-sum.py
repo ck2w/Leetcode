11c33b479fc95504b5e516adb3d338efc6c690bc
susie <susie@susiedeMacBook-Air.local>
2019-05-19 21:49:51 +0800

    [Normal] minor

--- py/0001.two-sum.py
+++ py/0001.two-sum.py
@@ -3,32 +3,6 @@
 #
 # [1] Two Sum
 #
-# https://leetcode.com/problems/two-sum/description/
-#
-# algorithms
-# Easy (41.89%)
-# Total Accepted:    1.5M
-# Total Submissions: 3.6M
-# Testcase Example:  '[2,7,11,15]\n9'
-#
-# Given an array of integers, return indices of the two numbers such that they
-# add up to a specific target.
-# 
-# You may assume that each input would have exactly one solution, and you may
-# not use the same element twice.
-# 
-# Example:
-# 
-# 
-# Given nums = [2, 7, 11, 15], target = 9,
-# 
-# Because nums[0] + nums[1] = 2 + 7 = 9,
-# return [0, 1].
-# 
-# 
-# 
-# 
-#
 # 1 brute force (5248ms)
 # loop possible combinations
 # class Solution:
